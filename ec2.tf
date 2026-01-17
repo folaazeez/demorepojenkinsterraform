5fd4d201b57b73d445464acacddf2be1dea6def0
Fola Azeez <foladevops@gmail.com>
2026-01-17 13:20:08 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -12,14 +12,10 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "terraform" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t2.micro"
-regio
-
   tags = {
     Name = "HelloWorld"
   }
 }
-
-mhfhchtlkwtgpofg
