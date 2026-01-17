8b4c71399de2f155f764f6ab34e3285705db741b
Fola Azeez <foladevops@gmail.com>
2026-01-17 13:17:05 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
 }
 
 resource "aws_instance" "example" {
